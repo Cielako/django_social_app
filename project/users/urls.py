0a44b1eba63e6e5e1e7496722ab11ff60f9bfa2e
Krzysztof Baczyński <k.baczynski200@gmail.com>
2024-11-16 14:13:10 +0100

    +user panel

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Widok logowania
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),  # Widok wylogowania
     path('register/', views.register, name='register'),
+    path('user/', views.user, name='user'),
 ]
--- project/users/views.py
+++ project/users/views.py
@@ -1,19 +1,23 @@
 from pyexpat.errors import messages
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 from users.forms import CustomUserCreationForm
 
-# Create your views here.
+@login_required  # Strona dla zalogowanych użytkowników
 def user(request):
-    return render(request, "users/user.html")
+    user = request.user  # pobranie zalogowanego użytkownika
+    return render(request, 'users/user.html', {'user': user})
 
 def register(request):
+    if request.user.is_authenticated:
+        # Jesli zalogowany, to nie może tworzyć konta i zostaje przekierowany na panel
+        return redirect('user')
+    
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-
-
             return redirect('index')  # Przekierowanie na stronę główną po rejestracji
     else:
         form = CustomUserCreationForm()
